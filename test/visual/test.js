5794fb17060abb51f310b874185dd36ee46e9172
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2019-03-27 16:45:14 +0200

    Add visual tests for the menu-bar

---
+++ test/visual/test.js
@@ -0,0 +1,41 @@
+gemini.suite('vaadin-menu-bar', function(rootSuite) {
+  function wait(actions, find) {
+    actions.wait(5000);
+  }
+
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
+
+  rootSuite
+    .before(wait)
+    .after(goToAboutBlank);
+
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`default-tests-${theme}`, function(suite) {
+      suite
+        .setUrl(`default.html?theme=${theme}`)
+        .setCaptureElements('#default-tests')
+        .capture('default', function(actions) {
+          actions.executeJS(function(window) {
+            window.openSubMenu();
+          });
+        });
+    });
+
+    gemini.suite(`overflow-tests-${theme}`, function(suite) {
+      suite
+        .setUrl(`overflow.html?theme=${theme}`)
+        .setCaptureElements('#overflow-tests')
+        .capture('overflow', function(actions) {
+          actions.executeJS(function(window) {
+            window.openSubMenu();
+          });
+        });
+    });
+  });
+
+});
